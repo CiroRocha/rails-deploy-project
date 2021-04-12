43ad3599290ac128936d92dc81dde843e7c27e7d
Ciro Rocha <o.cirorocha@gmail.com>
2021-04-12 15:42:28 -0300

    Create page to show all articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,9 @@
 class ArticlesController < ApplicationController
 
+  def index
+    @articles = Article.all
+  end
+
   def show
     # @ means it will be an instance variable, meaning it will be passed down to the view.
     # /articles/:id
