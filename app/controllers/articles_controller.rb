6980ad018d0e04669fcac4cc3a389e0c509bcf76
Ciro Rocha <o.cirorocha@gmail.com>
2021-04-18 13:42:34 -0300

    Article creation avaliable

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -10,4 +10,23 @@ class ArticlesController < ApplicationController
     @article = Article.find(params[:id])
   end
 
+  def new
+    @article = Article.new
+  end
+
+  def create
+    params.permit(:title, :description)
+    article_data = {
+      title: params[:title],
+      description: params[:description]
+    }
+    @article = Article.new(article_data)
+    if @article.save
+      flash[:notice] = "Article saved successfully"
+      redirect_to article_path(@article)
+    else
+      render 'new'
+    end
+  end
+
 end
