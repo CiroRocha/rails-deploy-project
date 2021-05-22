1083688cf05cdc57e630fd2dd50f67938859734e
Ciro Rocha <o.cirorocha@gmail.com>
2021-05-22 11:46:06 -0300

    Add next and styled-components

---
+++ react-app/pages/index.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+const Main = () => (
+  <div>
+    <Title>My First Next.js Page</Title>
+  </div>
+);
+
+const Title = styled.h1`
+  color: red;
+`;
+
+export default Main
