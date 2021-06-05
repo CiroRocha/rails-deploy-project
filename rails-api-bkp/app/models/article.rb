89546f1a7f178ffea6b8df125419f00cdbdf1be4
Ciro Rocha <o.cirorocha@gmail.com>
2021-06-05 20:08:44 -0300

    Delete backup rails project

--- rails-api-bkp/app/models/article.rb
+++
@@ -1,4 +0,0 @@
-class Article < ApplicationRecord
-  validates :title, presence: true, length: { minimum: 3, maximum: 150 }
-  validates :description, presence: true, length: { minimum: 50, maximum: 600 }
-end
--- rails-api-bkp/config/routes.rb
+++
@@ -1,7 +0,0 @@
-Rails.application.routes.draw do
-  root 'pages#home'
-  get 'about', to: 'pages#about'
-
-  # resources auto creates routes for REST/CRUD to a specific model
-  resources :articles
-end
