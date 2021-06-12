e1104c4f268a0fbe483b382611d27b82f858d2cb
Ciro Rocha <o.cirorocha@gmail.com>
2021-06-12 20:05:32 -0300

    ArticleForm

--- react-app/components/Form/ArticleForm/StyledArticleForm.js
+++ react-app/components/Form/ArticleForm/StyledArticleForm.js
@@ -1,3 +1,7 @@
 import styled from 'styled-components'
 
-export const StyledArticleForm = styled.form``
+export const StyledArticleForm = styled.form`
+  width: 90%;
+  max-width: 1200px;
+  margin: 0 auto;
+`
