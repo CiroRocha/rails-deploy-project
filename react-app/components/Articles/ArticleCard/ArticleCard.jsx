f4382586d36889904c74a5b21bcc98eb5375781f
Ciro Rocha <o.cirorocha@gmail.com>
2021-06-12 22:48:38 -0300

    Article Card

--- react-app/components/Articles/ArticleCard/ArticleCard.jsx
+++ react-app/components/Articles/ArticleCard/ArticleCard.jsx
@@ -11,23 +11,35 @@ import {
 
 import { StyledArticleCard } from './StyledArticleCard'
 
+import CommonButton from '../../Buttons/CommonButton/CommonButton'
+
 const ArticleCard = ({ title, description, createdAt, id, onDelete = () => {} }) => {
+  const descriptionToShow =
+    description.length <= 350 ? description : description.substr(0, 350) + '...'
+
   return (
     <StyledArticleCard>
-      <h4>{title}</h4>
-      <p>{description}</p>
-      <small>Created at: {createdAt}</small>
-      <Link href={buildFrontendArticlePathWithId(id)}>
-        <button>View</button>
-      </Link>
-      <Link href={buildFrontendEditArticlePathWithId(id)}>
-        <button>Edit</button>
-      </Link>
-      <button
-        onClick={() => axios.delete(buildApiArticlePathWithId(id)).then((res) => onDelete(res, id))}
-      >
-        Delete
-      </button>
+      <div className="article-card-preview-text">
+        <h4>{title}</h4>
+        <p>{descriptionToShow}</p>
+        <small>Created at: {createdAt}</small>
+      </div>
+      <div className="article-card-controls">
+        <Link href={buildFrontendArticlePathWithId(id)}>
+          <CommonButton backgroundColor="blue">View</CommonButton>
+        </Link>
+        <Link href={buildFrontendEditArticlePathWithId(id)}>
+          <CommonButton backgroundColor="blue">Edit</CommonButton>
+        </Link>
+        <CommonButton
+          backgroundColor="red"
+          onClick={() =>
+            axios.delete(buildApiArticlePathWithId(id)).then((res) => onDelete(res, id))
+          }
+        >
+          Delete
+        </CommonButton>
+      </div>
     </StyledArticleCard>
   )
 }
--- react-app/components/Articles/ArticleCard/StyledArticleCard.js
+++ react-app/components/Articles/ArticleCard/StyledArticleCard.js
@@ -1,8 +1,31 @@
 import styled from 'styled-components'
 
+import { rgba } from 'polished'
+
 export const StyledArticleCard = styled.div`
   width: 90%;
+  max-width: 1200px;
+  margin: 0 auto;
   display: flex;
   flex-direction: column;
   align-items: center;
+  background-color: ${({ theme }) => theme.foreground};
+  color: ${({ theme }) => theme.background};
+  padding: 30px;
+  box-shadow: 0;
+  border-radius: 15px;
+  transition: box-shadow 0.3s;
+
+  &:hover {
+    box-shadow: 0 5px 10px ${({ theme }) => rgba(theme.foreground, 0.3)};
+  }
+
+  .article-card-preview-text {
+    margin-bottom: 40px;
+    text-align: center;
+  }
+
+  .article-card-controls button:not(:last-of-type) {
+    margin-right: 10px;
+  }
 `
