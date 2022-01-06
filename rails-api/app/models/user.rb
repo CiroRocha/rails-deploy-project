5ca0612db7ac2601bf0f9f9ff062a601885c1f98
Ciro Rocha <o.cirorocha@gmail.com>
2022-01-06 20:38:45 -0300

    Add password field

--- rails-api/app/models/user.rb
+++ rails-api/app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
+
+  # Run before saving to DB
+  before_save { self.email = email.downcase }
+
   has_many :articles
   validates :username, presence: true,
                        length: {minimum: 3, maximum: 25},
@@ -8,5 +12,7 @@ class User < ApplicationRecord
                     length: {maximum: 255},
                     uniqueness: {case_sensitive: false},
                     format: {with: URI::MailTo::EMAIL_REGEXP}
+
+  has_secure_password
 
 end
