9b4b1e3f4ad96ec188d660533526cf21571e8e71
Ciro Rocha <o.cirorocha@gmail.com>
2022-01-05 22:30:35 -0300

    Validate user data

--- rails-api/app/models/user.rb
+++ rails-api/app/models/user.rb
@@ -1,3 +1,11 @@
 class User < ApplicationRecord
+  validates :username, presence: true,
+                       length: {minimum: 3, maximum: 25},
+                       uniqueness: {case_sensitive: false}
+
+  validates :email, presence: true,
+                    length: {maximum: 255},
+                    uniqueness: {case_sensitive: false},
+                    format: {with: URI::MailTo::EMAIL_REGEXP}
 
 end
